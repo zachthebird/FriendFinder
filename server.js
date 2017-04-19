f6a5af1f94364ad7f901ad4a75d9a26a7602de5e
Zach Bird <zachthebird@gmail.com>
2017-04-19 17:05:51 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+
+var path = require('path');
+
+var bodyParser = require('body-parser');
+
+var port = 4444;
+
+var app = express();
+
+
+require('./app/routing/htmlRoutes.js')(app, path);
+// require('./app/routing/apiRoutes.js')(app, path);
+
+
+app.listen(port);
