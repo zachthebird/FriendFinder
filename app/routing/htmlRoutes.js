69f3043192be2c734c6062776cf35468fc5e8148
Zach Bird <zachthebird@gmail.com>
2017-05-02 18:32:47 -0500

    heroku sterfff

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -10,8 +10,8 @@ module.exports = function (app) {
         res.sendFile(path.join(__dirname, '../public/survey.html'));
     });
 
-    app.use( function (req, res) {
-        res.sendFile(path.join(__dirname, '../public/home.html'))
-    });
+    // app.use( function (req, res) {
+    //     res.sendFile(path.join(__dirname, '../public/home.html'))
+    // });
 
 }
